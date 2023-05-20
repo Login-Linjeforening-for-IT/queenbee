0b7d6a8e237e30f98d33e18e1b213c788fc70e1f
DESKTOP-148T8UP\Simon <simonedna02@gmail.com>
2023-05-21 01:10:28 +0200

    Adjusted styles of markdown text

--- src/app/components/markdown-textfield/markdown-textfield.component.ts
+++ src/app/components/markdown-textfield/markdown-textfield.component.ts
@@ -9,26 +9,26 @@ export class MarkdownTextfieldComponent {
   angularVersion = VERSION.full;
   ngxMarkdownVersion = '16.0.0';
 
-  markdown = `## Markdown __rulez__!
-I morgen er det TekKom samling, med masse progging og pizza fråtsing! 😎
-
-Værmelding for dagen: [yr.no](https://www.yr.no)
-
-### Huskeliste
-1. Datautstyr
-   - Datamaskin
-   - Lader
-   - Mus
-2. Søvn, ikke lov å sove
-
-## English
-Coming later!
-
-| Name   | Age | City       |
-|--------|-----|------------|
-| John   | 25  | New York   |
-| Alice  | 30  | London     |
-| Peter  | 28  | San Francisco |
+  markdown = `## Markdown **rulez**!
+  Markdown er **veldig** *bra*! 😎
+  <br></br>
+  Værmelding for dagen: [yr.no](https://www.yr.no)
+  
+  ### Huskeliste
+  1. Datautstyr
+     - ~Datamaskin~
+     - Lader
+     - Mus
+  2. Søvn, ikke lov å sove
+  
+  ## English
+  Coming later!
+  
+  | Name   | Age | City       |
+  |--------|-----|------------|
+  | John   | 25  | New York   |
+  | Alice  | 30  | London     |
+  | Peter  | 28  | San Francisco |
 
 `;
 }
