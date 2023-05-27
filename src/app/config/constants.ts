e90ff7c792bf6dc2b95f5e36c18477780401f882
DESKTOP-148T8UP\Simon <simonedna02@gmail.com>
2023-05-27 12:02:12 +0200

    Created config file with URL constants

---
+++ src/app/config/constants.ts
@@ -0,0 +1,11 @@
+export const BeehiveAPI = {
+    BASE_URL: 'http://localhost:8080/api/v1',
+    
+    EVENTS_PATH: '/events',
+    JOBADS_PATH: '/jobs',
+    CATEGORIES_PATH: '/categories',
+    AUDIENCES_PATH: '/audiences',
+    ORGANIZATIONS_PATH: '/organizations',
+    RULES_PATH: '/rules',
+    LOCATIONS_PATH: '/locations'
+}
