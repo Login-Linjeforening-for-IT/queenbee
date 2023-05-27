ddc42fd3e2eadf96af1bc32ff40cfa14031050ed
DESKTOP-148T8UP\Simon <simonedna02@gmail.com>
2023-05-27 12:07:11 +0200

    Added working fetching of data from API

--- src/app/pages/events/data-table-event/data-table-event.component.ts
+++ src/app/pages/events/data-table-event/data-table-event.component.ts
@@ -1,23 +1,22 @@
-import { AfterViewInit, Component, ElementRef, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, ChangeDetectorRef, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
-import { DataTableEventDataSource, DataTableEventItem } from './data-table-event-datasource';
+import { DataTableEventDataSource } from './data-table-event-datasource';
 import { TableConstants } from 'src/app/pages/pages.constants';
 import { EventService } from 'src/app/services/api/event.service';
-import { Subscription } from 'rxjs';
+import { EventData } from 'src/app/models/event-data.model';
 
 @Component({
   selector: 'app-data-table-event',
   templateUrl: './data-table-event.component.html'
 })
-export class DataTableEventComponent implements AfterViewInit {
+export class DataTableEventComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-  @ViewChild(MatTable) table!: MatTable<DataTableEventItem>;
+  @ViewChild(MatTable) table!: MatTable<EventData>;
   @ViewChild('filterInput') filterInput!: ElementRef<HTMLInputElement>;
-  dataSource: DataTableEventDataSource;
-  subscription: Subscription | null = null;
+  dataSource!: DataTableEventDataSource;
   
   pageSize = TableConstants.PAGE_SIZE
   pageSizeOptions = TableConstants.PAGE_SIZE_OPTIONS
@@ -27,40 +26,35 @@ export class DataTableEventComponent implements AfterViewInit {
     'actions',
     'id',
     'name_no',
-    'canceled',
+    'category',
+    'location',
     'time_start',
     'time_end',
     'time_publish',
+    'capacity',
+    'full',
+    'canceled',
     'time_signup_release',
     'time_signup_deadline',
     'created_at',
-    'image_small',
-    'image_banner',
-    'link_facebook',
-    'link_discord',
-    'link_signup',
-    'digital',
-    'link_stream'
   ];
 
-  constructor(private changeDetectorRefs: ChangeDetectorRef, private eventsService: EventService) {
+  constructor(private eventsService: EventService, private cdr: ChangeDetectorRef) {
     this.dataSource = new DataTableEventDataSource(eventsService);
+  }
+
+  ngOnInit() {
+    this.dataSource.fetchEvents();
   }
 
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
     this.dataSource.filterStr = this.filterInput;
-    
-    // Use dataSource.data as the table's data source
-    this.subscription = this.dataSource.dataSubject.subscribe(events => {
-      this.table.dataSource = this.dataSource.data;
-      this.changeDetectorRefs.detectChanges();
-    });
-  }
 
-  ngOnDestroy(): void {
-    this.subscription?.unsubscribe();
+    // Use dataSource.data as the table's data source
+    this.table.dataSource = this.dataSource;
+    this.cdr.detectChanges();
   }
 
   onDelete(id: number): void {
@@ -75,5 +69,10 @@ export class DataTableEventComponent implements AfterViewInit {
       return dt.replace("T", " ").replace("Z", "").replaceAll("-", "/");
     }
     return dt
+  }
+
+  // Refreshes the table. Used when you need to force a refresh
+  refresh() {
+    this.paginator._changePageSize(this.paginator.pageSize);
   }
 }
