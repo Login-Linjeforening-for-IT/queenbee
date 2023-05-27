488c180f41e8865a51714fae002bf25c14a30b9e
DESKTOP-148T8UP\Simon <simonedna02@gmail.com>
2023-05-27 12:02:42 +0200

    Updated method to handle null/undefined values

--- src/app/common/utils.ts
+++ src/app/common/utils.ts
@@ -1,11 +1,18 @@
 /**
- * Function used to compare to values. If result is negative a should be places before b, and vice versa for
- * positive result.
+ * Function used to compare to values. If result is negative a should be placed before b, and vice versa for
+ * positive result. Handles null or undefined values - treats them as less than any other value.
  * @param a First value to compare
  * @param b Second value to compare
- * @param isAsc Indicates wether the comparison should be ascending order or descending
+ * @param isAsc Indicates whether the comparison should be ascending order or descending
  * @returns Number indicating ordering of a and b
  */
-export function compare(a: string | number, b: string | number, isAsc: boolean): number {
+export function compare(a: string | number | null | undefined, b: string | number | null | undefined, isAsc: boolean): number {
+    if (a === null || a === undefined) {
+      return isAsc ? -1 : 1;
+    }
+    if (b === null || b === undefined) {
+      return isAsc ? 1 : -1;
+    }
     return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
+  
