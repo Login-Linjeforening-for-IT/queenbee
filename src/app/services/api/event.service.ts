06d4d852c058aba81a6cf594648f861e392d560a
DESKTOP-148T8UP\Simon <simonedna02@gmail.com>
2023-05-27 12:20:41 +0200

    Added fetching of single event

--- src/app/services/api/event.service.ts
+++ src/app/services/api/event.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
+import { BeehiveAPI } from 'src/app/config/constants';
 import { EventData } from 'src/app/models/event-data.model';
 
 @Injectable({
@@ -10,13 +11,23 @@ export class EventService {
 
   constructor(private http: HttpClient) { }
 
-  fetchEvent() {
-
+  fetchEvent(eventId: number): Observable<EventData> {
+    return this.http
+      .get<EventData>(`${BeehiveAPI.BASE_URL}${BeehiveAPI.EVENTS_PATH}/${eventId}`)
+      .pipe(
+        map(resData => {
+          if (resData) {
+            const event: EventData = resData;
+            return event;
+          }
+          throw new Error('No event found with id ' + eventId);
+        })
+      );
   }
 
   fetchEvents(): Observable<EventData[]> {
     return this.http
-      .get<{ [id: string]: EventData }>('http://localhost:8080/api/v1/events')
+      .get<{ [id: string]: EventData }>(`${BeehiveAPI.BASE_URL}${BeehiveAPI.EVENTS_PATH}`)
       .pipe(
         map(resData => {
           const eventsArray: EventData[] = [];
