faca9e1680f63cee6d85006d83e7769373676352
DESKTOP-148T8UP\Simon <simonedna02@gmail.com>
2023-09-12 22:59:20 +0200

    Finished layer switching feature

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -23,42 +23,37 @@ export class MapComponent implements AfterViewInit {
   };
 
   private initMap(): void {
-    this.map = L.map('map', {
-      center: [ 60.79039181292657, 10.683486407725253 ], // Coords of a special room
-      zoom: 13
-    });
-
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
-      minZoom: 3,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
 
-    const satelliteTiles = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png', {
+    var osmHot = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'
+    });
+
+
+    const esriSat = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png', {
       maxZoom: 18,
       minZoom: 3,
       attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
     });
 
-    tiles.addTo(this.map);
-
-    const toggleButton = document.getElementById('toggleMapView') as HTMLButtonElement;
-
-    toggleButton.addEventListener('click', () => {
-      if (this.isSatelliteView) {
-        // Switch to OpenStreetMap view
-        this.map.removeLayer(satelliteTiles);
-        tiles.addTo(this.map);
-        toggleButton.textContent = 'Toggle Satellite View';
-      } else {
-        // Switch to Satellite view
-        this.map.removeLayer(tiles);
-        satelliteTiles.addTo(this.map);
-        toggleButton.textContent = 'Toggle Map View';
-      }
-
-      this.isSatelliteView = !this.isSatelliteView;
+    this.map = L.map('map', {
+      center: [ 60.79039181292657, 10.683486407725253 ], // Coords of a special room
+      zoom: 13,
+      layers: [osm]
     });
+
+    var baseMaps = {
+      "OpenStreetMap": osm,
+      "OpenStreetMap.HOT": osmHot,
+      "ESRI Sattelite": esriSat
+    };
+
+    var layerControl = L.control.layers(baseMaps).addTo(this.map);
+
 
     this.map.on("click", (e: { latlng: { lat: number; lng: number; }; }) => {
       this.coords.emit({lat: '' + e.latlng.lat, long: '' + e.latlng.lng})
