e952cfca6b9dd38cce363604e27d005de000041c
eirikhanasand <eirik.hanasand@gmail.com>
2024-09-04 14:16:00 +0200

    Added SAML env variable

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,15 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth/auth.service';
+import { Component } from '@angular/core'
+import { Router } from '@angular/router'
+import { AuthService } from 'src/app/services/auth/auth.service'
+import { config } from 'dotenv'
+
+config()
+
+const { SAML_URL } = process.env
+
+if ( !SAML_URL ) {
+    throw new Error('Missing SAML_URL environment variable.')
+}
 
 @Component({
   selector: 'app-login',
@@ -8,17 +17,17 @@ import { AuthService } from 'src/app/services/auth/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  feedback: string = '';
+    feedback: string = ''
 
-  constructor(private authService: AuthService, private router: Router) {}
+    constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit() {
-    // Initiate SAML login redirect on component load or based on user action
-    this.redirectToSaml();
-  }
+    ngOnInit() {
+        // Initiate SAML login redirect on component load or based on user action
+        this.redirectToSaml();
+    }
 
-  // Function to redirect user to the SAML SSO endpoint
-  redirectToSaml() {
-    window.location.href = 'https://authentik.login.no/application/saml/queenbee/sso/binding/init/';
-  }
+    // Function to redirect user to the SAML SSO endpoint
+    redirectToSaml() {
+        window.location.href = SAML_URL || 'https://login.no/404';
+    }
 }
